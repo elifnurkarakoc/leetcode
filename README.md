cb2cb11f62ff4a680e0018915769a9557690391e
elifnurkarakoc <elifnurkarakoc@gmail.com>
2023-10-27 22:49:28 +0300

    twoSum solution added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # leetcode
+
+
+| # | Title | Solution | Difficulty |
+|---| ----- | -------- | ---------- |
+| 1 | Two Sum |  [✅](./solutions/typescript/twoSum/twoSum.ts) | Easy |
---
+++ solutions/typescript/twoSum/twoSum.ts
@@ -0,0 +1,36 @@
+/**
+ * 
+ * Question: 
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * 
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * 
+ */
+
+/**
+ * 
+ * Solution: 
+ *
+ * In this solution, each item in the array checks the rule. 
+ * If the sum of two different items in the array is equal to target. The return of the function is equals their indices. 
+ * 
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function (nums: number[], target: number): number[] | undefined {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
